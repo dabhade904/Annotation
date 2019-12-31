1afa8201e6b67fa4f167cd5a1f8c39b468f7f334
dabhade904 <dabhade904@gmail.com>
2019-12-31 16:49:01 +0530

    Added : Annotation problem

---
+++ src/main/java/com/bridgelabz/annotation/MethodInfo.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.METHOD)
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MethodInfo {
+    String author() default "Sachin";
+    String date();
+    int revision() default 1;
+    String comments();
+}
